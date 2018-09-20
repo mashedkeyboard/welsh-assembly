2a765a10c9e9d11f3a95c0644e5bfcb0dda7e3e9
Tony Bowden <tony@tmtm.com>
2018-09-20 11:34:47 +0100

    Turn off archiving
    Part of https://github.com/everypolitician/everypolitician/issues/630

--- scraper.rb
+++ scraper.rb
@@ -6,9 +6,8 @@ require 'pry'
 require 'scraped'
 require 'scraperwiki'
 
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
-require 'scraped_page_archive/open-uri'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
 
 @ConstituencyRegion = {}
 
